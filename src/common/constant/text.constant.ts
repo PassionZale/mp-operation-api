ff8b0255cbe70f052f417d1b5a7db8af861e42af
张磊 <zhanglei@rainbowcn.com>
2020-09-12 17:40:57 +0800

    字符串脱敏工具函数

--- src/common/constant/text.constant.ts
+++ src/common/constant/text.constant.ts
@@ -6,3 +6,7 @@ export const DEFAULT_ERROR_MESSAGE = '网络开小差';
  * 默认成功提示语
  */
 export const DEFAULT_SUCCESS_MESSAGE = '操作成功';
+/**
+ * 字符串脱敏遮罩
+ */
+export const SENSITIVE_MASK = '**********';
---
+++ src/common/helper/sensitive.helper.ts
@@ -0,0 +1,14 @@
+import { SENSITIVE_MASK } from "@src/common/constant/text.constant";
+
+/**
+ * 字符串脱敏工具函数
+ * @param s 需要脱敏的字符串
+ * @param beginIndex 从哪个索引开始
+ * @param endIndex 以哪个索引结束
+ */
+export function desensitization(s: string, beginIndex = 1, endIndex = -1): string {
+  const left = s.slice(0, beginIndex);
+  const right = s.slice(endIndex);
+
+  return `${left}${SENSITIVE_MASK}${right}`;
+}
