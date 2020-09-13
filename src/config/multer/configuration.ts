bb0ad8c919b016f398c8f045c808087b4f9afab7
张磊 <zhanglei@rainbowcn.com>
2020-09-13 21:27:40 +0800

    pipeline 和 public 模块上传 multer options 配置

--- src/config/multer/configuration.ts
+++ src/config/multer/configuration.ts
@@ -39,15 +39,27 @@ export const multerConfig = {
   public: {
     root: './media/public',
     fileSize: 20 * 1024 * 1024,
-    files: 1,
+    files: 5,
   }
 }
+
+
+/**
+ * getAvatarMulterOptions, getPipelineMulterOptions, getPublicMulterOptions
+ * 可以更优雅的封装为一个 Class, 但是! 此处需要返回一个 MulterOptions, 并且! 需要在装饰器中注入，例如：
+ * @UseInterceptors(
+ *  FileInterceptor('file', getAvatarMulterOptions()),
+ * )
+ * public async upload() {...}
+ * 
+ * #TODO 单独扩展 FileInterceptor, 将下下方函数封装为类，集成至拦截器中
+ */
 
 // 用户头像上传
 export const getAvatarMulterOptions = (): MulterOptions =>  {
   return {
     /**
-     * 文件大小上传限制
+     * 文件大小、数量限制
      */
     limits: {
       fileSize: multerConfig.avatar.fileSize,
@@ -116,11 +128,145 @@ export const getAvatarMulterOptions = (): MulterOptions =>  {
 }
 
 // 流水线压缩包上传
-export const getPipelineMulterOptions = (): string => {
-  return '';
+export const getPipelineMulterOptions = (): MulterOptions =>  {
+  return {
+    /**
+     * 文件大小、数量限制
+     */
+    limits: {
+      fileSize: multerConfig.pipeline.fileSize,
+      files: multerConfig.pipeline.files,
+    },
+    /**
+     * 文件格式校验
+     */
+    fileFilter: (
+      req: Request,
+      file: Express.Multer.File,
+      cb: multerOptionsCallBack,
+    ): void => {
+      if (file.mimetype.match(/\/(zip))$/)) {
+        cb(null, true);
+      } else {
+        cb(
+          new ApiException(
+            '只能上传 zip 类型的文件',
+            ApiErrorCode.FILE_MIMETYPE_INVALID,
+          ),
+          false,
+        );
+      }
+    },
+    /**
+     * 文件存储方式
+     */
+    storage: diskStorage({
+      /**
+       * 存储目录: `${root}/${year}/${month}/${date}`
+       * 例如：./media/2020/09/13
+       */
+      destination: async (
+        req: Request,
+        file: Express.Multer.File,
+        cb: multerDiskStorageDestinationCallBack,
+      ): Promise<void> => {
+        const root = multerConfig.pipeline.root;
+
+        const year = moment().get('y');
+
+        const m = moment().get('M') + 1;
+        const month = m > 10 ? m : `0${m}`;
+        
+        const date = moment().get('D');
+
+        const fullPath = `${root}/${year}/${month}/${date}`;
+
+        // 如果目录不存在，则创建目录
+        if (!existsSync(fullPath)) {
+          await mkdirp(fullPath);
+        }
+
+        cb(null, fullPath);
+      },
+      filename: (
+        req: Request,
+        file: Express.Multer.File,
+        cb: multerDiskStorageFilenameCallBack,
+      ) => {
+        cb(null, `${uuid()}${extname(file.originalname)}`);
+      },
+    }),
+  };
 }
 
 // 公共资源上传，如：富文本图片、分类图片等等
-export const getPublicMulterOptions = ():string => {
-  return '';
+export const getPublicMulterOptions = (): MulterOptions =>  {
+  return {
+    /**
+     * 文件大小、数量限制
+     */
+    limits: {
+      fileSize: multerConfig.public.fileSize,
+      files: multerConfig.public.files,
+    },
+    /**
+     * 文件格式校验
+     */
+    fileFilter: (
+      req: Request,
+      file: Express.Multer.File,
+      cb: multerOptionsCallBack,
+    ): void => {
+      if (file.mimetype.match(/\/(jpg|jpeg|png|gif|mp4|zip))$/)) {
+        cb(null, true);
+      } else {
+        cb(
+          new ApiException(
+            '只能上传 jpg|jpeg|png|gif|mp4|zip 类型的文件',
+            ApiErrorCode.FILE_MIMETYPE_INVALID,
+          ),
+          false,
+        );
+      }
+    },
+    /**
+     * 文件存储方式
+     */
+    storage: diskStorage({
+      /**
+       * 存储目录: `${root}/${year}/${month}/${date}`
+       * 例如：./media/2020/09/13
+       */
+      destination: async (
+        req: Request,
+        file: Express.Multer.File,
+        cb: multerDiskStorageDestinationCallBack,
+      ): Promise<void> => {
+        const root = multerConfig.pipeline.root;
+
+        const year = moment().get('y');
+
+        const m = moment().get('M') + 1;
+        const month = m > 10 ? m : `0${m}`;
+        
+        const date = moment().get('D');
+
+        const fullPath = `${root}/${year}/${month}/${date}`;
+
+        // 如果目录不存在，则创建目录
+        if (!existsSync(fullPath)) {
+          await mkdirp(fullPath);
+        }
+
+        cb(null, fullPath);
+      },
+      filename: (
+        req: Request,
+        file: Express.Multer.File,
+        cb: multerDiskStorageFilenameCallBack,
+      ) => {
+        cb(null, `${uuid()}${extname(file.originalname)}`);
+      },
+    }),
+  };
 }
