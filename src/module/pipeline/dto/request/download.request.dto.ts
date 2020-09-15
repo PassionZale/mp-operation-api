90f9806234ee648e5134d81e5d8a22706741b8d8
张磊 <zhanglei@rainbowcn.com>
2020-09-15 20:11:02 +0800

    nothing

--- src/module/pipeline/dto/request/download.request.dto.ts
+++ src/module/pipeline/dto/request/download.request.dto.ts
@@ -1,12 +1,10 @@
 import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class DownloadRequestDto {
-
-  deploy_id: number
   @IsUUID(4, { message: '流水线序号格式有误，应为 uuid v4' })
   @IsNotEmpty({ message: '流水线序号不能为空' })
   readonly pipeline_id: string;
 
   @IsNotEmpty({ message: '部署日志 ID 不能为空' })
-  readonly deoloy_id: number;
+  readonly deploy_id: number;
 }
--- src/module/pipeline/pipeline.module.ts
+++ src/module/pipeline/pipeline.module.ts
@@ -6,6 +6,8 @@ import { PipeLineReleaseLogEntity } from './entity/pipeline-release-log.entity';
 import { PipeLineService } from './pipeline.service';
 import { PipeLineController } from './pipeline.controller';
 import { AppConfigModule } from '@src/config/app/config.module';
+import { PipeLineDeployService } from './pipeline-deploy.service';
+import { PipeLineDeployController } from './pipeline-deploy.controller';
 
 @Module({
   imports: [
@@ -16,8 +18,8 @@ import { AppConfigModule } from '@src/config/app/config.module';
     ]),
     AppConfigModule
   ],
-  providers: [PipeLineService],
-  controllers: [PipeLineController],
-  exports: [PipeLineService],
+  providers: [PipeLineService, PipeLineDeployService],
+  controllers: [PipeLineController, PipeLineDeployController],
+  exports: [PipeLineService, PipeLineDeployService],
 })
 export class PipeLineModule {}
