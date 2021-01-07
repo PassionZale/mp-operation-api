16561d34dd786093209584c57c96b2a20bb45850
PassionZale <whouu@qq.com>
2021-01-07 23:14:15 +0800

    update: 新增 miniprogram/:id/pipeline/:pid API route

--- src/module/miniprogram/miniprogram.controller.ts
+++ src/module/miniprogram/miniprogram.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { ProjectService } from '../project/project.service';
 import { PipeLineService } from '../pipeline/service/pipeline.service';
 import { ProjectEntity } from '../project/project.entity';
+import { PipeLineEntity } from '../pipeline/entity/pipeline.entity';
 
 @Controller()
 export class MiniProgramController {
@@ -9,6 +10,13 @@ export class MiniProgramController {
     private readonly projectService: ProjectService,
     private readonly pipelineService: PipeLineService,
   ) {}
+
+  @Get('miniprogram/:id/pipeline/:pid')
+  public async findPipeline(@Param('id') id:number, @Param('pid') pid: number): Promise<PipeLineEntity> {
+    const pipeline = await this.pipelineService.findPipelineDeloyLogs(id, pid)
+
+    return pipeline
+  }
 
   @Get('miniprogram/:id')
   public async findOne(@Param() id:number): Promise<ProjectEntity> {
